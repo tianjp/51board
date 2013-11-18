04790465a0e2d25046c85a48e47072a452d3bcb8
Tian Jianping <281998302@qq.com>
2013-11-18 22:04:31 +0800

    test input pin
    Signed-off-by: Tian Jianping <281998302@qq.com>

--- main.c
+++ main.c
@@ -6,25 +6,28 @@
 
 bit busy;
 
+sbit PIN0 = P2^0;           //up pin
+sbit PIN1 = P2^1;           //down pin
+
 void SendData(char dat);
 void SendString(char *s);
 
 void Uart_Init()
 {
-    SCON = 0x50;            //8-bit variable UART
+	SCON = 0x50;            //8-bit variable UART
 
-    TMOD = 0x20;            //Set Timer1 as 8-bit auto reload mode
-    TH1 = TL1 = -(FOSC/12/32/BAUD); //Set auto-reload vaule
-    TR1 = 1;                //Timer1 start run
-    ES = 1;                 //Enable UART interrupt
-    EA = 1;                 //Open master interrupt switch
+	TMOD = 0x20;            //Set Timer1 as 8-bit auto reload mode
+	TH1 = TL1 = -(FOSC/12/32/BAUD); //Set auto-reload vaule
+	TR1 = 1;                //Timer1 start run
+	ES = 1;                 //Enable UART interrupt
+	EA = 1;                 //Open master interrupt switch
 }
 
 void Uart_Isr() interrupt 4 using 1
 {
     if (TI)
     {
-        TI = 0;             //Clear transmit interrupt flag
+		TI = 0;             //Clear transmit interrupt flag
         busy = 0;           //Clear transmit busy flag
     }
 }
@@ -66,11 +69,30 @@ void Delay1ms(unsigned int cnt)		//@11.0592MHz
 void main()
 {
 	Uart_Init();
+	SendString("Uart Test!\r\n");
+	
 	while (1)
 	{
-		SendString("Uart Test!\r\n");
-		Delay1ms(1000);
+		if (!PIN0)
+		{
+			Delay1ms(100); 
+			if (!PIN0)
+			{
+				SendString("up\r\n");
+			}
+		}
+		else if (!PIN1)
+		{
+			Delay1ms(100); 
+			if (!PIN1)
+			{
+				SendString("down\r\n");
+			}
+		}
+
+		Delay1ms(10);
 	}
+
 }
 
 
